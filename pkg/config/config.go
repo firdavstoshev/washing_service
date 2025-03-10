a634ed5860691f40eeafd84270c7338995091774
Firdavs Toshev <firdavs080902@gmail.com>
2025-03-10 14:12:31 +0500

    feat: move sensitive data to env

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,6 +1,12 @@
 package config
 
-import "github.com/spf13/viper"
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/spf13/viper"
+)
 
 type PostgresConfig struct {
 	Host     string `mapstructure:"host"`
@@ -21,19 +27,42 @@ type Config struct {
 }
 
 func Init() (*Config, error) {
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found, using system environment variables")
+	}
+
 	viper.SetConfigName("config")
 	viper.SetConfigType("yaml")
 	viper.AddConfigPath(".")
-
-	err := viper.ReadInConfig()
-	if err != nil {
-		return nil, err
+	if err := viper.ReadInConfig(); err != nil {
+		log.Printf("Failed to read config.yml: %v", err)
 	}
 
 	var cfg Config
-	err = viper.Unmarshal(&cfg)
-	if err != nil {
+	if err := viper.Unmarshal(&cfg); err != nil {
 		return nil, err
+	}
+
+	if port := os.Getenv("SERVER_PORT"); port != "" {
+		cfg.Server.Port = port
+	}
+	if host := os.Getenv("POSTGRES_HOST"); host != "" {
+		cfg.Postgres.Host = host
+	}
+	if dbname := os.Getenv("POSTGRES_DBNAME"); dbname != "" {
+		cfg.Postgres.DBName = dbname
+	}
+	if user := os.Getenv("POSTGRES_USER"); user != "" {
+		cfg.Postgres.User = user
+	}
+	if password := os.Getenv("POSTGRES_PASSWORD"); password != "" {
+		cfg.Postgres.Password = password
+	}
+	if port := os.Getenv("POSTGRES_PORT"); port != "" {
+		cfg.Postgres.Port = viper.GetInt("postgres.port")
+	}
+	if sslMode := os.Getenv("POSTGRES_SSL_MODE"); sslMode != "" {
+		cfg.Postgres.SSLMode = sslMode
 	}
 
 	return &cfg, nil
